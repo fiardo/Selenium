ff96da78ca3180801257321e5279a7f746864904
apple <apple@apples-MacBook-Pro.local>
2023-05-15 18:10:40 +0530

    service name

--- conftest.py
+++ conftest.py
@@ -27,7 +27,7 @@ def setup(request):
     global driver
     browser_name = request.config.getoption("browser_name")
     if browser_name == "chrome":
-        s = Service("C:/Users/TUL/Desktop/cd/cd4.exe")
+        s = Service("/Users/apple/Desktop/My\Data/cd")
         driver = webdriver.Chrome(service=s)
         driver.get('https://www.universityliving.com/')
         wait = 1
